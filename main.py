f429cd3a7e172b65386e8355e1708dea4edf8492
Xianguang-Zhou <xianguang.zhou@outlook.com>
2018-05-23 20:13:02 +0800

    update window title by "window-title-changed" signal

--- main.py
+++ main.py
@@ -34,13 +34,9 @@ __license__ = 'AGPL-3.0'
 
 
 class Terminal(Vte.Terminal):
-    no_name_title = '[No Name]'
-
     def __init__(self):
         Vte.Terminal.__init__(self)
         self._is_vim_inited = False
-        self._nvim_buffer_name = Terminal.no_name_title
-        self._nvim_buffer_dir_path = ''
         self._nvim_listen_address = os.path.join(GLib.get_tmp_dir(),
                                                  'nvim_' + str(uuid.uuid4()))
         self._cursor_moved_handler_id = self.connect('cursor-moved',
@@ -79,33 +75,10 @@ class Terminal(Vte.Terminal):
     def _on_nvim_notification(self, event, args):
         if event == 'Gui':
             first_arg = args[0]
-            if first_arg == 'Changed':
-                GLib.idle_add(self._notify_changed, *args[1:])
-            elif first_arg == 'Buffer':
-                GLib.idle_add(self._notify_buffer, *args[1:])
-            elif first_arg == 'Font':
+            if first_arg == 'Font':
                 GLib.idle_add(self._notify_font, *args[1:])
             elif first_arg == 'Color':
                 GLib.idle_add(self._notify_color, *args[1:])
-
-    def _notify_changed(self, buf_changed):
-        buf_changed = ' +' if buf_changed else ''
-        title = self._nvim_buffer_name + buf_changed + self._nvim_buffer_dir_path
-        self.emit('title-changed', title)
-
-    def _notify_buffer(self, buf_path, buf_changed):
-        if len(buf_path) == 0:
-            self._nvim_buffer_name = Terminal.no_name_title
-            self._nvim_buffer_dir_path = ''
-        else:
-            self._nvim_buffer_dir_path, self._nvim_buffer_name = os.path.split(
-                buf_path)
-            self._nvim_buffer_dir_path = ' (' + self._nvim_buffer_dir_path + ')'
-        self._notify_changed(buf_changed)
-
-    @GObject.Signal()
-    def title_changed(self, title: str):
-        pass
 
     def _notify_font(self, font_str):
         font_family, *font_attrs = font_str.split(':')
@@ -141,8 +114,7 @@ class Terminal(Vte.Terminal):
 class Window(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self)
-        app_name = 'Nvim'
-        self.set_title(Terminal.no_name_title + ' - ' + app_name)
+        self.set_title('NVIM')
         self.set_icon_from_file(
             os.path.join(os.path.dirname(__file__), 'icon', 'neovim.svg'))
         terminal = Terminal()
@@ -150,8 +122,8 @@ class Window(Gtk.Window):
         terminal.connect('child-exited',
                          lambda _status, _user_data: self.destroy())
         terminal.connect(
-            'title-changed',
-            lambda _terminal, title: self.set_title(title + ' - ' + app_name))
+            'window-title-changed',
+            lambda terminal: self.set_title(terminal.get_window_title()))
         self.connect('delete-event', terminal.on_window_delete)
 
 
--- runtime/plugin/light_neovim_gtk.vim
+++ runtime/plugin/light_neovim_gtk.vim
@@ -36,51 +36,6 @@ function s:GuiColor() abort
 endfunction
 autocmd ColorScheme * call s:GuiColor()
 
-let s:buf_text_changed = 0
-function s:GuiBufTextChangedI() abort
-  if !s:buf_text_changed
-    let s:buf_text_changed = 1
-    call rpcnotify(g:gui_channel, 'Gui', 'Changed', 1)
-  endif
-endfunction
-autocmd TextChangedI * call s:GuiBufTextChangedI()
-function s:GuiBufWrited() abort
-  if s:buf_text_changed
-    let s:buf_text_changed = 0
-    call rpcnotify(g:gui_channel, 'Gui', 'Changed', 0)
-  endif
-endfunction
-autocmd BufWritePost * call s:GuiBufWrited()
-function s:GuiBufTextChanged() abort
-  let buf_changed = getbufinfo('%')[0]['changed']
-  if buf_changed
-    call s:GuiBufTextChangedI()
-  else
-    call s:GuiBufWrited()
-  endif
-endfunction
-autocmd TextChanged * call s:GuiBufTextChanged()
-
-let s:last_buf_path = ''
-function s:GuiBufEntered() abort
-  let buf_name = bufname('%')
-  let buf_info = getbufinfo('%')[0]
-  let buf_changed = buf_info['changed']
-  if buf_name == ''
-    let buf_path = ''
-  else
-    let buf_path = fnamemodify(buf_info['name'], ':~')
-  endif
-  if s:last_buf_path != buf_path || s:buf_text_changed != buf_changed
-    let s:last_buf_path = buf_path
-    let s:buf_text_changed = buf_changed
-    call rpcnotify(g:gui_channel, 'Gui', 'Buffer', buf_path, buf_changed)
-  endif
-endfunction
-autocmd BufEnter,WinEnter,DirChanged,TermOpen * call s:GuiBufEntered()
-if v:vim_did_enter
-  call s:GuiBufEntered()
-else
-  autocmd VimEnter * call s:GuiBufEntered()
-endif
+set title
+set termguicolors
 
