d3c302a67ced3d5d980898848da9c4a05b5919f4
Xianguang-Zhou <xianguang.zhou@outlook.com>
2018-05-24 15:15:20 +0800

    change "install.sh" and "light_neovim_gtk.desktop"

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ SCRIPT_PATH=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPT_DIR_PATH=$(dirname "$SCRIPT_PATH")
 
-ln -i -s $SCRIPT_DIR_PATH/run.sh ~/.local/bin/gnvim
+ln -f -s $SCRIPT_DIR_PATH/run.sh ~/.local/bin/light-neovim-gtk
 cp -f $SCRIPT_DIR_PATH/icon/light_neovim_gtk.svg ~/.local/share/icons/
 cp -f $SCRIPT_DIR_PATH/light_neovim_gtk.desktop ~/.local/share/applications/
+ln -i -s $SCRIPT_DIR_PATH/run.sh ~/.local/bin/gnvim
 
